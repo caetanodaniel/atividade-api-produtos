e0f529651323c2a782f872e362a929f13cc3a961
caetanodaniel <daniel.caetano@ba.estudante.senai.br>
2026-09-22 18:31:14 +0000

    Produtos Eletronicos

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,33 @@ api.use(cors())
 api.use(express.json())
 
 api.get('/', (request, response) => {
-    response.status(200).json({
-        "Nome": "PC Gamer ",
-        "Preco": 10000,
-        "Categoria": "Eletrônicos"
-    })
+
+response.status(200).json([
+
+    {
+        nome: 'Xbox One',
+        preco: 2500,
+        categoria: 'Eletronicos'
+    },
+
+    {
+        nome: 'Monitor Gamer',
+        preco: 3000,
+        categoria: 'Tablet'
+    },
+
+    {
+        nome: 'Controle de Ps4',
+        preco: 150,
+        categoria: 'Acessorios'
+    }
+
+])
+
 })
+
 api.listen(3000, () => {
-    console.log('Api ta funcionando');
+
+    console.log('api ta funcionando');
+
 })
--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,22 @@
-
-const URL = 'http://localhost:3000/';
+const URL = 'https://super-computing-machine-xrv7pgppwv9wc6rxv-3000.app.github.dev/';
 
 async function carregarDados() {
     const resposta = await fetch(URL);
     const produtos = await resposta.json();
 
-       const listaProdutos = document.getElementById('lista-produtos');
+    const listaProdutos = document.getElementById('lista-produtos');
 
+    listaProdutos.innerHTML = '';
+
+   produtos.forEach(produto => {
+    listaProdutos.innerHTML += `
+        <div class="card">
+            <h2>${produto.nome}</h2>
+            <p>${produto.categoria}</p>
+            <p class="preco">R$ ${produto.preco.toLocaleString('pt-BR')}</p>
+        </div>
+    `;
+});
 }
 
-  
 carregarDados();
